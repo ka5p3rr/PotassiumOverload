13a4c2113cf9721c122e0cd189abe16b23da9ca7
Andrea Cerasoni <andreacerasoni@gmail.com>
2019-10-23 22:00:14 +0100

    Pushing empty libgdx project

---
+++ src/desktop/src/com/cucubananas/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cucubananas.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cucubananas.core.PotassiumOverload;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PotassiumOverload(), config);
+	}
+}
