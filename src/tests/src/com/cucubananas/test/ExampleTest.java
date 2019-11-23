43975226624225af356b0043b0f33b9e616bc185
Aidan Reed <psyar8@nottingham.ac.uk>
2019-11-23 20:10:53 +0000

    Introduced unit testing and travis ci
    Added GDX wrapper to perform unit tests with LibGDX based on: https://github.com/TomGrill/gdx-testing
    Added travis ci file to compile and run test project

---
+++ src/tests/src/com/cucubananas/test/ExampleTest.java
@@ -0,0 +1,23 @@
+package com.cucubananas.test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import de.tomgrill.gdxtesting.GdxTestRunner;
+
+@RunWith(GdxTestRunner.class)
+public class ExampleTest {
+
+  @Test
+  public void oneEqualsOne() {
+    assertEquals(1, 1);
+  }
+
+  @Test
+  public void checkAssetLogoExists() {
+    assertTrue("Checks default LibGDX logo is in assets folder", Gdx.files.internal("../core/assets/badlogic.jpg").exists());
+  }
+
+}
