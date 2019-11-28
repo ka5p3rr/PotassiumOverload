b4ec7c8dafc2f491baa6aa2e33de188891e9d808
Alexandros Araouzos <32824479+al-exisar@users.noreply.github.com>
2019-11-28 15:02:48 +0000

    added architecture for movable object including player enemy bullet
    # Write one line commit summary above (72 characters)
    # All lines starting with # are ignored
    # Write detailed description below:
    
    # Keep two lines before listing co-authors
    
    
    # In case of pair coding remove # for any of the following co-authors
    # Co-authored-By: ka5p3rr <32355403+ka5p3rr@users.noreply.github.com>
    # Co-authored-by: acerasoni <32521086+acerasoni@users.noreply.github.com>
    # Co-authored-by: confiscar <24923564+confiscar@users.noreply.github.com>
    # Co-authored-by: al-exisar <32824479+al-exisar@users.noreply.github.com>
    Co-authored-by: psyar8 <37447000+psyar8@users.noreply.github.com>

--- PotassiumOverload/core/src/com/cucubananas/core/PotassiumOverload.java
+++ PotassiumOverload/core/src/com/cucubananas/core/PotassiumOverload.java
@@ -5,29 +5,33 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.cucubananas.core.actor.Enemy;
+import com.cucubananas.core.actor.Player;
 
 public class PotassiumOverload extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Stage stage;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage();
+		Player player = new Player();
+		Enemy enemy = new Enemy();
+		stage.addActor(enemy);
+		stage.addActor(player);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		float delta = Gdx.graphics.getDeltaTime();
+		stage.act(delta);
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
+
 }
