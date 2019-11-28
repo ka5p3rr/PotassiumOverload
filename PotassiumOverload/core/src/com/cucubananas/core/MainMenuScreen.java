029155976cda456d30d77cc81c21a75869f0ea48
Francisco Caeiro <24923564+confiscar@users.noreply.github.com>
2019-11-28 13:31:56 +0000

    Added blank main menu screen class
    Added a generic screen interface for the main menu screen.
    
    Co-authored-By: ka5p3rr <32355403+ka5p3rr@users.noreply.github.com>

---
+++ PotassiumOverload/core/src/com/cucubananas/core/MainMenuScreen.java
@@ -0,0 +1,47 @@
+package com.cucubananas.core;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+
+  final PotassiumOverload game;
+
+  public MainMenuScreen(PotassiumOverload game) {
+    this.game = game;
+  }
+
+  @Override
+  public void show() {
+
+  }
+
+  @Override
+  public void render(float delta) {
+
+  }
+
+  @Override
+  public void resize(int width, int height) {
+
+  }
+
+  @Override
+  public void pause() {
+
+  }
+
+  @Override
+  public void resume() {
+
+  }
+
+  @Override
+  public void hide() {
+
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+}
