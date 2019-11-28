938af32bf72292f67163be75394410da6f284baa
Francisco Caeiro <24923564+confiscar@users.noreply.github.com>
2019-11-28 14:01:12 +0000

    Added a simple main menu screen
    Adds a camera and font drawing into the main menu class. Added a default field for the font of the game in the PotassiumOverload class. Also changed PotassiumOverload extends from ApplicationListener to Game.
    
    Co-authored-By: ka5p3rr <32355403+ka5p3rr@users.noreply.github.com>

--- PotassiumOverload/core/src/com/cucubananas/core/MainMenuScreen.java
+++ PotassiumOverload/core/src/com/cucubananas/core/MainMenuScreen.java
@@ -1,13 +1,28 @@
 package com.cucubananas.core;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class MainMenuScreen implements Screen {
 
   final PotassiumOverload game;
+  OrthographicCamera camera;
+  int width = 800;
+  int height = 480;
+  boolean blink;
+  int blinkConstant;
+  int blinkCounter;
 
   public MainMenuScreen(PotassiumOverload game) {
     this.game = game;
+    this.blink = false;
+    this.blinkConstant = 40;
+    this.blinkCounter = blinkConstant;
+    camera = new OrthographicCamera();
+    camera.setToOrtho(false, width, height);
   }
 
   @Override
@@ -17,7 +32,30 @@ public class MainMenuScreen implements Screen {
 
   @Override
   public void render(float delta) {
+    Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+    camera.update();
+    game.batch.setProjectionMatrix(camera.combined);
+
+    game.batch.begin();
+    game.font.draw(game.batch, "Potassium Overload", width / 2 - 70, height / 2 + 100);
+    blinkCounter--;
+    if (blinkCounter <= 0) {
+      blinkCounter = blinkConstant;
+      blink = !blink;
+    }
+    if (!blink) {
+      game.font.draw(game.batch, "Press space to begin!", width / 2 - 75, height / 2);
+    }
+
+    game.batch.end();
+
+    if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+      System.out.println("DEBUG");
+      //game.setScreen(new GameScreen(game)); this will be the game screen
+      //dispose();
+    }
   }
 
   @Override
