ce62c28dc823dcc07603169230c3a376a09551bb
Aidan Reed <psyar8@nottingham.ac.uk>
2019-12-02 12:36:35 +0000

    Changed the path for player asset
    Using Gdx internal file library caused an error when retriving the file

--- PotassiumOverload/core/src/com/cucubananas/core/actor/Player.java
+++ PotassiumOverload/core/src/com/cucubananas/core/actor/Player.java
@@ -1,7 +1,5 @@
 package com.cucubananas.core.actor;
 
-import com.badlogic.gdx.Gdx;
-
 /**
  * Represents the main player object that is controlled by the player.
  * @author Aidan Reed
@@ -10,7 +8,7 @@ import com.badlogic.gdx.Gdx;
 public class Player extends MoveableObject {
 
   public Player(int x, int y) {
-    super(Gdx.files.internal("../core/assets/bird.png").path(), x, y);
+    super("bird.png", x, y);
   }
 
   @Override
