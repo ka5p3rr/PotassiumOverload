349556597be9898690a2b47f944db09ae3b445a8
David Mares <32355403+ka5p3rr@users.noreply.github.com>
2019-12-02 12:24:05 +0000

    Add key logging
    Added key press logging for later use.
    
    Co-authored-by: acerasoni <32521086+acerasoni@users.noreply.github.com>

--- PotassiumOverload/core/src/com/cucubananas/core/PotassiumOverload.java
+++ PotassiumOverload/core/src/com/cucubananas/core/PotassiumOverload.java
@@ -6,54 +6,59 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.cucubananas.core.actor.Enemy;
-import com.cucubananas.core.actor.Player;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PotassiumOverload extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
+  Logger logger = Logger.getLogger(PotassiumOverload.class.getName());
+  SpriteBatch batch;
+  Texture img;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+  @Override
+  public void create() {
+    batch = new SpriteBatch();
+    img = new Texture("badlogic.jpg");
+  }
+
+  @Override
+  public void render() {
+    Gdx.gl.glClearColor(1, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      // MOVE UP ON Y AXIS
+      logger.log(Level.INFO, "UP");
+    } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      logger.log(Level.INFO, "DOWN");
+      // MOVE DOWN ON Y AXIS
+    } else {
+      // MOVE DOWN ON Y AXIS BY DEFAULT
+      logger.log(Level.INFO, "GRAVITY DOWN");
     }
 
-    @Override
-    public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            // MOVE UP ON Y AXIS
-        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            // MOVE DOWN ON Y AXIS
-        } else {
-            // MOVE DOWN ON Y AXIS BY DEFAULT
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            // CHANGE ORIENTATION TO LEFT
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            // CHANGE ORIENTATION TO RIGHT
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            // FIRE BULLET
-        }
-
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+      // CHANGE ORIENTATION TO LEFT
+      logger.log(Level.INFO, "LEFT");
     }
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
+    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+      // CHANGE ORIENTATION TO RIGHT
+      logger.log(Level.INFO, "RIGHT");
     }
 
+    if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+      // FIRE BULLET
+      logger.log(Level.INFO, "SPACE");
+    }
+
+    batch.begin();
+    batch.draw(img, 0, 0);
+    batch.end();
+  }
+
+  @Override
+  public void dispose() {
+    batch.dispose();
+    img.dispose();
+  }
 }
