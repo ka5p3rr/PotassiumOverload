3163c481edd19a83da25c14983476b4eb6661d6c
Andrea Cerasoni <andreacerasoni@gmail.com>
2019-12-03 11:17:21 +0000

    Adding assets for unit tests

--- PotassiumOverload/tests/src/com/cucubananas/test/PlayerTests.java
+++ PotassiumOverload/tests/src/com/cucubananas/test/PlayerTests.java
@@ -1,5 +1,6 @@
 package com.cucubananas.test;
 
+import com.cucubananas.core.actor.Player;
 import de.tomgrill.gdxtesting.GdxTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,28 +8,28 @@ import org.junit.runner.RunWith;
 @RunWith(GdxTestRunner.class)
 public class PlayerTests {
 
-  @Test
-  public void checkDefaultCoordinates() {
+    @Test
+    public void checkDefaultCoordinates() {
+        Player player = new Player("bird.png", 50, 50);
+    }
 
-  }
+    @Test
+    public void checkYBottomCoordinates() {
 
-  @Test
-  public void checkYBottomCoordinates() {
+    }
 
-  }
+    @Test
+    public void checkYTopCoordinates() {
 
-  @Test
-  public void checkYTopCoordinates() {
+    }
 
-  }
+    @Test
+    public void checkXRightCoordinates() {
 
-  @Test
-  public void checkXRightCoordinates() {
+    }
 
-  }
+    @Test
+    public void checkXLeftCoordinates() {
 
-  @Test
-  public void checkXLeftCoordinates() {
-
-  }
+    }
 }
