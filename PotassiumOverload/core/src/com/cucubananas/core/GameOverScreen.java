53dc80ff09a422f74b4b08ba4f0ed43e73cdb91c
Andrea Cerasoni <andreacerasoni@gmail.com>
2019-12-07 16:30:04 +0000

    Fix GameOverScreen not extending AbstractScreen

--- PotassiumOverload/core/src/com/cucubananas/core/GameOverScreen.java
+++ PotassiumOverload/core/src/com/cucubananas/core/GameOverScreen.java
@@ -1,50 +1,9 @@
 package com.cucubananas.core;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
+public class GameOverScreen extends AbstractScreen {
 
-public class GameOverScreen implements Screen {
-  private PotassiumOverload game;
-  public GameOverScreen(PotassiumOverload game) {
-    this.game = game;
-  }
-
-  @Override
-  public void show() {
-
-  }
-
-  @Override
-  public void render(float delta) {
-    Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-  }
-
-  @Override
-  public void resize(int width, int height) {
-
-  }
-
-  @Override
-  public void pause() {
-
-  }
-
-  @Override
-  public void resume() {
-
-  }
-
-  @Override
-  public void hide() {
-
-  }
-
-  @Override
-  public void dispose() {
-
-  }
+    public GameOverScreen(PotassiumOverload game) {
+        super(game);
+    }
 
 }
