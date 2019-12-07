836d9a9e04f413c0fe4fa38211fc5e2abbd92eb6
Andrea Cerasoni <andreacerasoni@gmail.com>
2019-12-07 15:40:02 +0000

    Removing generics

--- PotassiumOverload/core/src/com/cucubananas/core/GameScreen.java
+++ PotassiumOverload/core/src/com/cucubananas/core/GameScreen.java
@@ -3,11 +3,7 @@ package com.cucubananas.core;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.cucubananas.core.actor.MoveableObject;
 import com.cucubananas.core.actor.Player;
@@ -15,7 +11,7 @@ import com.cucubananas.core.actor.Player;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class GameScreen<BitmapFont> extends AbstractScreen {
+public class GameScreen extends AbstractScreen {
 
     private Logger logger = Logger.getLogger(PotassiumOverload.class.getName());
     private CustomStage stage;
@@ -70,6 +66,7 @@ public class GameScreen<BitmapFont> extends AbstractScreen {
 
         stage.updateHitboxes();
 
+        System.out.println(player.checkCollision(p2));
         //TODO uncomment once Projectile and Collectibles are implemented
         /*
         stage.checkProjectilesCollision();
